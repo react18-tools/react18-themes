830ecdf77bd8b8517ca1f4ef8231ea0c98cc4c03
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-08 17:19:26 +0530

    working so far

--- examples/remix/app/routes/_index.tsx
+++ examples/remix/app/routes/_index.tsx
@@ -1,3 +1,12 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "Remix Example" },
+		{ name: "description", content: "Welcome to React18 Themes with Remix!" },
+	];
+};
+
 export default function Home() {
 	return <div>Remix Example</div>;
 }
--- examples/remix/remix.config.js
+++ examples/remix/remix.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+	ignoredRouteFiles: ["**/.*"],
+	watchPaths: ["../../packages/react18-themes"],
+	transpileModules: ["react18-themes"],
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// publicPath: "/build/",
+	// serverBuildPath: "build/index.js",
 };
