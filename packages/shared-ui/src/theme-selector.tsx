577f618e0824a29c48e9b77a1e93e6a3ef7da1ef
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-13 22:10:59 +0530

    attempt tp reduce cognitive complexity

--- packages/shared-ui/src/theme-selector.tsx
+++ packages/shared-ui/src/theme-selector.tsx
@@ -11,15 +11,7 @@ interface ThemeSelectorProps {
 }
 
 export function ThemeSelector({ scope }: ThemeSelectorProps) {
-	const [colorSchemePref, theme, setTheme] = useTheme(state => {
-		let _theme = state.theme;
-		let _setTheme = state.setTheme;
-		if (scope !== "") {
-			_theme = scope === "dark" ? state.darkTheme : state.lightTheme;
-			_setTheme = scope === "dark" ? state.setDarkTheme : state.setLightTheme;
-		}
-		return [state.colorSchemePref, _theme, _setTheme];
-	});
+	const { colorSchemePref, theme, setTheme } = useThemeStates(scope);
 	const themes = useMemo(() => {
 		switch (scope) {
 			case "":
@@ -30,10 +22,14 @@ export function ThemeSelector({ scope }: ThemeSelectorProps) {
 				return lightThemes;
 		}
 	}, [scope]);
+
 	useEffect(() => {
 		setTheme(themes[0]);
 	}, [setTheme, themes]);
+
 	let className = colorSchemePref ? "" : styles.active;
+
+	/** handle className for dark and light themes */
 	if (scope !== "") {
 		if (colorSchemePref === scope) className = styles.active;
 		else className = colorSchemePref === "system" ? styles[scope] : "";
@@ -45,4 +41,18 @@ export function ThemeSelector({ scope }: ThemeSelectorProps) {
 			Select {scope} theme <Select className={className} onChange={handleChange} options={themes} value={theme} />
 		</p>
 	);
+}
+
+function useThemeStates(scope: ThemeSelectorProps["scope"]) {
+	const [colorSchemePref, theme, setTheme] = useTheme(state => {
+		switch (scope) {
+			case "":
+				return [state.colorSchemePref, state.theme, state.setTheme];
+			case "dark":
+				return [state.colorSchemePref, state.darkTheme, state.setDarkTheme];
+			case "light":
+				return [state.colorSchemePref, state.lightTheme, state.setLightTheme];
+		}
+	});
+	return { colorSchemePref, theme, setTheme };
 }
