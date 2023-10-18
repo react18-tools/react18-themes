97f59d07f02f08920090c68f8000a80198d0a3fe
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-18 10:39:27 +0530

    add shared layout to vite

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
