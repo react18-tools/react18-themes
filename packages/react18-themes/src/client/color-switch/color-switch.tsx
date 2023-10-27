cab9911fd5a62b992ce60c74fe541d890e3510cb
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-27 18:42:51 +0530

    add color switch

--- packages/react18-themes/src/client/color-switch/color-switch.tsx
+++ packages/react18-themes/src/client/color-switch/color-switch.tsx
@@ -1,18 +1,38 @@
 import * as React from "react";
+import { useTheme } from "../../store";
 
 interface ColorSwitchProps {
-	children?: React.ReactNode;
+	size?: number;
 }
 
 /**
  * # ColorSwitch
  * Color switch button to quickly set user preference
  */
-export function ColorSwitch({ children }: ColorSwitchProps) {
+export function ColorSwitch({ size = 25 }: ColorSwitchProps) {
+	const [colorSchemePref, setColorSchemePref] = useTheme(state => [state.colorSchemePref, state.setColorSchemePref]);
+	const toggleColorScheme = () => {
+		switch (colorSchemePref) {
+			case "":
+			case "system":
+				setColorSchemePref("dark");
+				break;
+			case "dark":
+				setColorSchemePref("light");
+				break;
+			case "light":
+				setColorSchemePref("system");
+		}
+	};
 	return (
-		<div>
-			<h1 data-testid="color-switch-h1">color switch</h1>
-			{children}
-		</div>
+		<div
+			className="react18-themes--color-switch"
+			onClick={toggleColorScheme}
+			onKeyUp={e => e.key === "Enter" && toggleColorScheme()}
+			role="button"
+			// @ts-expect-error -- setting custom attribute
+			style={{ "--size": `${size}px` }}
+			tabIndex={0}
+		/>
 	);
 }
