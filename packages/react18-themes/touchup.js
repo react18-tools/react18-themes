fe704698924456ba0ed7a4783ca63839b6130891
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-11-24 17:15:50 +0530

    Create GitHub release on publish

--- packages/react18-themes/touchup.js
+++ packages/react18-themes/touchup.js
@@ -4,7 +4,37 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const packageJson = require(path.resolve(process.cwd(), "package.json"));
+if (process.env.TOKEN) {
+	const { Octokit } = require("octokit");
+	// Octokit.js
+	// https://github.com/octokit/core.js#readme
+	const octokit = new Octokit({
+		auth: process.env.TOKEN,
+	});
 
+	const octoOptions = {
+		owner: process.env.OWNER,
+		repo: process.env.REPO,
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	};
+	const tagName = `v${packageJson.version}`;
+	const name = `Release ${tagName}`;
+	/** Create a release */
+	octokit.request("POST /repos/{owner}/{repo}/releases", {
+		...octoOptions,
+		tag_name: tagName,
+		target_commitish: "main",
+		name,
+		draft: false,
+		prerelease: false,
+		generate_release_notes: true,
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	});
+}
 delete packageJson.devDependencies;
 delete packageJson.scripts;
 
