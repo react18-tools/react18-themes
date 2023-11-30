5f3d46dd434ebdaf0ca64a25cfaf74be41a91f80
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-11-30 13:40:02 +0530

    add docs link

--- packages/shared-ui/src/cards/index.tsx
+++ packages/shared-ui/src/cards/index.tsx
@@ -3,11 +3,13 @@ import type { PageNavigatorCardProps } from "./page-navigator-card";
 import { PageNavigatorCard } from "./page-navigator-card";
 import styles from "./cards.module.css";
 import { StarMeCard } from "./star-me-card";
+import { DocsCard } from "./docs-card";
 
 export function Cards({ LinkElement }: PageNavigatorCardProps) {
 	return (
 		<div className={styles.cards}>
 			<PageNavigatorCard LinkElement={LinkElement} />
+			<DocsCard />
 			<ExamplesCard />
 			<StarMeCard />
 		</div>
