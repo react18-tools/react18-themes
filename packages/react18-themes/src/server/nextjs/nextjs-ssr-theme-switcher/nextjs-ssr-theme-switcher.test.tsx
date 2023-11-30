92c75d8608f128cbff90c1c7974dc91a446fbfa6
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-11-30 09:29:32 +0530

    fix edge cases and update tests

--- packages/react18-themes/src/server/nextjs/nextjs-ssr-theme-switcher/nextjs-ssr-theme-switcher.test.tsx
+++ packages/react18-themes/src/server/nextjs/nextjs-ssr-theme-switcher/nextjs-ssr-theme-switcher.test.tsx
@@ -5,82 +5,63 @@ import { NextJsSSRThemeSwitcher } from "./nextjs-ssr-theme-switcher";
 describe("nextjs-ssr-theme-switcher", () => {
 	afterEach(cleanup);
 
-	test.concurrent("test tag", ({ expect }) => {
-		render(
-			<NextJsSSRThemeSwitcher>
-				<body>ServerSideWrapper</body>
-			</NextJsSSRThemeSwitcher>,
-		);
-		expect(screen.getByTestId("nextjs-ssr-theme-switcher").tagName).toBe("HTML");
+	beforeEach(() => {
+		globalThis.cookies = {
+			"react18-themes": {
+				value: JSON.stringify({
+					theme: "yellow",
+					darkTheme: "dark-blue",
+					lightTheme: "light-yellow",
+					colorSchemePref: "dark",
+				}),
+			},
+			"data-color-scheme-system": { value: "dark" },
+		};
+		globalThis.path = "";
 	});
-	test.concurrent("test custom tag", ({ expect }) => {
-		render(<NextJsSSRThemeSwitcher tag="div">ServerSideWrapper</NextJsSSRThemeSwitcher>);
+
+	test("test tag", ({ expect }) => {
+		globalThis.cookies = {};
+		render(<NextJsSSRThemeSwitcher />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").tagName).toBe("DIV");
 	});
-	test.concurrent("forced theme", ({ expect }) => {
+	test("test custom tag", ({ expect }) => {
+		render(<NextJsSSRThemeSwitcher tag="main" />);
+		expect(screen.getByTestId("nextjs-ssr-theme-switcher").tagName).toBe("MAIN");
+	});
+	test("forced theme", ({ expect }) => {
 		globalThis.path = "/themed-page/dark1";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/themed-page\/dark1/, { theme: "dark1" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/themed-page\/dark1/, { theme: "dark1" }]]} />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("dark1");
 	});
 
 	/** test cookies and forced pages */
-	globalThis.cookies = {
-		"data-theme-dark": { value: "dark-blue" },
-		"data-theme-light": { value: "light-yellow" },
-		"data-theme": { value: "yellow" },
-		"data-color-scheme": { value: "dark" },
-		"data-color-scheme-pref": { value: "dark" },
-	};
 	test("forced color scheme dark", ({ expect }) => {
 		globalThis.path = "/forced-color-scheme/dark";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/dark/, { colorScheme: "dark" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/dark/, { colorScheme: "dark" }]]} />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("dark-blue");
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-color-scheme")).toBe("dark");
 	});
 	test("forced color scheme light", ({ expect }) => {
 		globalThis.path = "/forced-color-scheme/light";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/light/, { colorScheme: "light" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/light/, { colorScheme: "light" }]]} />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("light-yellow");
 	});
 	test("forced color scheme system", ({ expect }) => {
 		globalThis.path = "/forced-color-scheme/system";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/system/, { colorScheme: "system" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme\/system/, { colorScheme: "system" }]]} />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("dark-blue");
 	});
 	test("force disable color scheme", ({ expect }) => {
 		globalThis.path = "/forced-color-scheme";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme$/, { colorScheme: "" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme$/, { colorScheme: "" }]]} />);
 		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("yellow");
 	});
 	/** forced page but no cookies */
 	test("force disable color scheme", ({ expect }) => {
 		globalThis.cookies = {};
 		globalThis.path = "/forced-color-scheme";
-		render(
-			<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme$/, { colorScheme: "" }]]} tag="div">
-				ServerSideWrapper
-			</NextJsSSRThemeSwitcher>,
-		);
-		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-theme")).toBe("");
+		render(<NextJsSSRThemeSwitcher forcedPages={[[/forced-color-scheme$/, { colorScheme: "" }]]} />);
+		expect(screen.getByTestId("nextjs-ssr-theme-switcher").getAttribute("data-color-scheme")).toBe("");
 	});
 });
--- packages/react18-themes/src/server/nextjs/nextjs-ssr-theme-switcher/nextjs-ssr-theme-switcher.tsx
+++ packages/react18-themes/src/server/nextjs/nextjs-ssr-theme-switcher/nextjs-ssr-theme-switcher.tsx
@@ -20,51 +20,52 @@ export function NextJsSSRThemeSwitcher({ children, tag, forcedPages, ...props }:
 	const Tag: keyof JSX.IntrinsicElements = tag || "div";
 
 	const state = cookies().get("react18-themes")?.value;
-	/** Return early if no state is found */
-	// @ts-expect-error -> svg props and html element props conflict
-	if (!state) return <Tag {...props}>{children}</Tag>;
 
 	const path = headers().get("referer");
 	const forcedPageData = forcedPages?.find(forcedPage => path?.match(forcedPage[0]));
 
-	const themeState = JSON.parse(state) as ThemeStoreType;
+	const themeState = state ? (JSON.parse(state) as ThemeStoreType) : undefined;
 	const isSystemDark = cookies().get("data-color-scheme-system")?.value === "dark";
 	const { dataTheme, dataColorScheme } =
 		forcedPageData === undefined
 			? getTheme(themeState, isSystemDark)
 			: getForcedPageTheme(themeState, forcedPageData, isSystemDark);
 
+	const dataProps: { "data-theme"?: string; "data-color-scheme"?: ColorSchemeType } = {};
+	if (dataTheme !== undefined) dataProps["data-theme"] = dataTheme;
+	if (dataColorScheme !== undefined) dataProps["data-color-scheme"] = dataColorScheme;
+
 	return (
 		// @ts-expect-error -> svg props and html element props conflict
-		<Tag
-			id="react18-themes"
-			data-color-scheme={dataColorScheme}
-			data-theme={dataTheme}
-			{...props}
-			data-testid="nextjs-ssr-theme-switcher">
+		<Tag id="react18-themes" {...dataProps} {...props} data-testid="nextjs-ssr-theme-switcher">
 			{children}
 		</Tag>
 	);
 }
 
 interface Theme {
-	dataTheme: string;
-	dataColorScheme: string;
+	dataTheme?: string;
+	dataColorScheme?: ColorSchemeType;
 }
 
-function getTheme(themeState: ThemeStoreType, isSystemDark: boolean): Theme {
+function getTheme(themeState: ThemeStoreType | undefined, isSystemDark: boolean): Theme {
+	if (!themeState) return {};
 	const dataTheme = resolveThemeFromColorScheme(themeState, isSystemDark);
 	return { dataTheme, dataColorScheme: themeState.colorSchemePref };
 }
 
-function getForcedPageTheme(themeState: ThemeStoreType, forcedPageData: ForcedPage, isSystemDark: boolean): Theme {
+function getForcedPageTheme(
+	themeState: ThemeStoreType | undefined,
+	forcedPageData: ForcedPage,
+	isSystemDark: boolean,
+): Theme {
 	const dataColorScheme =
-		forcedPageData[1].colorScheme === undefined ? themeState.colorSchemePref : forcedPageData[1].colorScheme;
+		forcedPageData[1].colorScheme === undefined ? themeState?.colorSchemePref : forcedPageData[1].colorScheme;
 
 	let dataTheme;
 	if (forcedPageData[1].theme) {
 		dataTheme = forcedPageData[1].theme;
-	} else {
+	} else if (dataColorScheme !== undefined && themeState !== undefined) {
 		dataTheme = resolveThemeFromColorScheme({ ...themeState, colorSchemePref: dataColorScheme }, isSystemDark);
 	}
 	return { dataTheme, dataColorScheme };
