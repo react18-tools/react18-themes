a8852daabc3b87df19c923e78db1ccc2556e225d
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-14 09:51:13 +0530

    touch up

--- packages/shared-ui/src/cards/index.tsx
+++ packages/shared-ui/src/cards/index.tsx
@@ -1,16 +1,19 @@
-import { ExamplesCard } from "./examples-card";
 import type { PageNavigatorCardProps } from "./page-navigator-card";
 import { PageNavigatorCard } from "./page-navigator-card";
 import styles from "./cards.module.css";
 import { StarMeCard } from "./star-me-card";
-import { DocsCard } from "./docs-card";
+import { LinkCard } from "./link-card";
 
 export function Cards({ LinkElement }: PageNavigatorCardProps) {
 	return (
 		<div className={styles.cards}>
 			<PageNavigatorCard LinkElement={LinkElement} />
-			<DocsCard />
-			<ExamplesCard />
+			<LinkCard href="https://react18-tools.github.io/react18-themes/" text="Explore the official docs." title="Docs" />
+			<LinkCard
+				href="https://github.com/react18-tools/react18-themes"
+				text="Explore more examples on official GitHub Repo."
+				title="More Examples"
+			/>
 			<StarMeCard />
 		</div>
 	);
