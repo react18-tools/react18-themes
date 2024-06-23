d2ec2ddd17703cfe1f1de04f025e9e13d87cabcd
Mayank <mayank.srmu@gmail.com>
2024-06-23 13:52:18 +0530

    Fix tests

--- lib/src/server/remix/remix-server-target/remix-server-target.test.tsx
+++ lib/src/server/remix/remix-server-target/remix-server-target.test.tsx
@@ -1,23 +1,11 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { afterEach, describe, test } from "vitest";
-import { createRemixStub } from "@remix-run/testing";
-import { json } from "@remix-run/node";
 import { RemixServerTarget } from "./remix-server-target";
-
-const RemixStub = createRemixStub([
-  {
-    path: "/",
-    Component: RemixServerTarget,
-    loader() {
-      return json({ dataProps: { "data-theme": "dark", "data-color-scheme": "dark" } });
-    },
-  },
-]);
 
 describe.concurrent("remix-server-target", () => {
   afterEach(cleanup);
 
-  test("", ({ expect }) => {
-    render(<RemixStub />);
+  test("renders without errors", () => {
+    render(<RemixServerTarget />);
   });
 });
