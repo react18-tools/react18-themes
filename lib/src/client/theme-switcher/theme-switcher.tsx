865ffd4719392cbc073b6546527ef56d02cd0bb5
Mayank <mayank.srmu@gmail.com>
2024-06-25 03:36:01 +0530

    remove inline source plugin

--- lib/src/client/theme-switcher/theme-switcher.tsx
+++ lib/src/client/theme-switcher/theme-switcher.tsx
@@ -74,8 +74,6 @@ const modifyTransition = (themeTransition = "none") => {
  * ```tsx
  * <ThemeSwitcher />
  * ```
- *
- * @source - Source code
  */
 export const ThemeSwitcher = ({
   forcedTheme,
